8bedad3497e9c43915c923d5ccd96663ab6e2b32
Dan Pinto <fozzarelo@gmail.com>
2016-07-04 13:20:58 -0700

    fixed a big in the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 
 User.create	first_name: "admin",
-				email:		"q@q.q",
-				password: "admin",
-				admin: true
+        email:		"q@q.q",
+        password: "admin",
+        admin: true
 
 6.times do
    User.create	first_name: Faker::Name.first_name,
-								last_name: 	Faker::Name.last_name,
-								email:		Faker::Internet.email,
-								password: "p"
+                last_name: 	Faker::Name.last_name,
+                email:		Faker::Internet.email,
+                password: "p"
 
 end
 
@@ -22,27 +22,27 @@ Category.create(title: "Life")
 Category.create(title: "Economy")
 
 30.times do
-  Post.create  	:  		Faker::Book.title,
-              	body:    		Faker::Lorem.paragraph,
+  Post.create  	title:  		Faker::Book.title,
+                body:    		Faker::Lorem.paragraph,
                 user_id: 		rand(5)+1
 end
 
 90.times do
-	Comment.create	 body: 			Faker::Lorem.paragraph,
-						       user_id: 	rand(5)+1,
-									 post_id:		rand(29)+1
+  Comment.create	 body: 			Faker::Lorem.paragraph,
+                   user_id: 	rand(5)+1,
+                   post_id:		rand(29)+1
 end
 
 60.times do
-	Tagging.create(post_id: rand(29)+1, category_id: rand(7)+1)
+  Tagging.create(post_id: rand(29)+1, category_id: rand(7)+1)
 end
 
 60.times do
-	Like.create(post_id: rand(29)+1, user_id: rand(5)+1)
+  Like.create(post_id: rand(29)+1, user_id: rand(5)+1)
 end
 
 60.times do
-	Fav.create(post_id: rand(29)+1, user_id: rand(5)+1)
+  Fav.create(post_id: rand(29)+1, user_id: rand(5)+1)
 end
 
 puts "Seeded"
